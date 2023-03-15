4fff090545b6078335db090ecd9f8a0a77440e34
Kobzpik <prabath.pik2299@gmail.com>
2023-03-15 23:05:36 +0530

    intergrate rest_framework

--- search/urls.py
+++ search/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path,include
-from .import views
+from .views import ElasticsearchView
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'user', ElasticsearchView ,basename='MyModel')
 
 urlpatterns = [
-    path('test', views.test_elasticsearch,name="test_elasticsearch")
+    #path('test', views.test_elasticsearch,name="test_elasticsearch"),
+    path('test/', ElasticsearchView.as_view()),
+    #path('test1/', include(router.urls)),
 ]
 
