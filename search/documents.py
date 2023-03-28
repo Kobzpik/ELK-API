92c8668dc9b6d45a249b4bc36e8c3e687c1747b3
Kobzpik <prabath.pik2299@gmail.com>
2023-03-28 20:35:08 +0530

    intergrate with real logs

--- search/documents.py
+++ search/documents.py
@@ -7,18 +7,102 @@ from search.models import Search
 @registry.register_document
 class SearchDocument(Document):
     
-    title = fields.TextField(
-        attr='title',
+    TimeStamp = fields.TextField(
+        attr='TimeStamp',
         fields={
             'raw': fields.TextField(),
             'suggest': fields.CompletionField(),
         }
     )
-    category = fields.ObjectField(
-        attr='category',
-        properties={
-            'id': fields.IntegerField(),
-            'title': fields.TextField(),
+    From = fields.TextField(
+        attr='From',
+        fields={
+            'raw': fields.TextField(),
+            'suggest': fields.CompletionField(),
+        }
+    )
+    To = fields.TextField(
+        attr='To',
+        fields={
+            'raw': fields.TextField(),
+            'suggest': fields.CompletionField(),
+        }
+    )
+    Body = fields.TextField(
+        attr='Body',
+        fields={
+            'raw': fields.TextField(),
+            'suggest': fields.CompletionField(),
+        }
+    )
+    Status = fields.TextField(
+        attr='Status',
+        fields={
+            'raw': fields.TextField(),
+            'suggest': fields.CompletionField(),
+        }
+    )
+    SentDate = fields.TextField(
+        attr='SentDate',
+        fields={
+            'raw': fields.TextField(),
+            'suggest': fields.CompletionField(),
+        }
+    )
+    ApiVersion = fields.TextField(
+        attr='ApiVersion',
+        fields={
+            'raw': fields.TextField(),
+            'suggest': fields.CompletionField(),
+        }
+    )
+    NumSegments = fields.TextField(
+        attr='NumSegments',
+        fields={
+            'raw': fields.TextField(),
+            'suggest': fields.CompletionField(),
+        }
+    )
+    AccountSid = fields.TextField(
+        attr='AccountSid',
+        fields={
+            'raw': fields.TextField(),
+            'suggest': fields.CompletionField(),
+        }
+    )
+    Sid = fields.TextField(
+        attr='Sid',
+        fields={
+            'raw': fields.TextField(),
+            'suggest': fields.CompletionField(),
+        }
+    )
+    AccountSid = fields.TextField(
+        attr='AccountSid',
+        fields={
+            'raw': fields.TextField(),
+            'suggest': fields.CompletionField(),
+        }
+    )
+    Direction = fields.TextField(
+        attr='Direction',
+        fields={
+            'raw': fields.TextField(),
+            'suggest': fields.CompletionField(),
+        }
+    )
+    Price = fields.TextField(
+        attr='Price',
+        fields={
+            'raw': fields.TextField(),
+            'suggest': fields.CompletionField(),
+        }
+    )
+    PriceUnit = fields.TextField(
+        attr='PriceUnit',
+        fields={
+            'raw': fields.TextField(),
+            'suggest': fields.CompletionField(),
         }
     )
 
